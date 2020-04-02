a25392bef2abe8124dc3123fea78850ab5d5da23
Kamille Longalong <k.pagibigan@gmail.com>
2020-04-02 08:36:49 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,7 +7,7 @@ def create_project_hash
   
   projects = {}
   
-  kickstarter.css("li.project.grid_4").each do 
+  kickstarter.css("li.project.grid_4").each do |project|
 end
 
 create_project_hash
