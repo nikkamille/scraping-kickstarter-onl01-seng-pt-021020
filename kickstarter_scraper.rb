98111b6b5f8606b9bf376a51fb4764f8b56c4508
Kamille Longalong <k.pagibigan@gmail.com>
2020-04-02 08:30:06 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,3 +14,4 @@ create_project_hash
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text.strip
 # location: project.css("ul.project-meta li a").text.strip
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i 
