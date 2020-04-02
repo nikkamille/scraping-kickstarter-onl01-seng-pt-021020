6643acea838b3fab9e250afb4a748b2f9f8108f8
Kamille Longalong <k.pagibigan@gmail.com>
2020-04-02 08:40:22 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,7 +12,7 @@ def create_project_hash
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text.strip,
-      :location => 
+      :location => project.css("ul.project-meta li a").text.strip
     }
     
     
